fe92190abc164aecc000a0ed503d4cfea5d487b0
Guglielmo2008 <gulymez08@gmail.com>
2025-05-13 12:43:48 +0200

    salvtaggio prova 1

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-window_size = (500, 500)
+import pygame 
+import random
+import sys
+
+pygame.init()
+WIDTH, HEIGHT = 600,800
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+ciano = (0, 255, 255)
+rosso =(255, 0, 0)
+magenta = (230, 150, 255)
+font = pygame.font.SysFont("Arial", 30)
+clock= pygame.time.Clock()
+fps = 60
+
+navicella = pygame.Surface(50, 40)
+navicella.fill(ciano)
+velocita_navicella = 5
+proiettile = pygame.Surface(5, 10)
+proiettile.fill(magenta)
+velocita_proiettile = 7
+nemico= pygame.Surface(30, 20)
+nemico.fill(rosso)
+velocita_nemico= 3
+
+class Navicella:
+    def __init__(self):
+        self.rect = navicella.get_rect(centro = (WIDTH //2, HEIGHT - 60))
+    
+    def movimento(self, sdx):
+        self.rect.x = sdx * velocita_navicella
+        self.rect.x = max(0, min(WIDTH - self.rect.width, self.rect.x))
+
+         
